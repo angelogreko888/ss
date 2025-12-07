726bcb165c27591422568c6aedbf22e01da9f70b
angelogreko <angelogreko@gmail.com>
2025-12-07 12:50:54 +0200

    commit

--- bin/bin/gstatus.sh
+++ bin/bin/gstatus.sh
@@ -7,5 +7,7 @@
 	rsync search-history.db  ~/ss/freetube/.config/FreeTube/search-history.db
 	rsync settings.db  ~/ss/freetube/.config/FreeTube/settings.db
 
+	rsync /usr/bin/ss ~/ss/no/ss
+
 	cd ~/ss
 	git status
