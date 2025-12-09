324eb403ee66c3a667144a0e52ec9ec34c6ed7cc
angelogreko <angelogreko@gmail.com>
2025-12-09 16:56:56 +0200

    commit

--- bin/bin/poweroff.sh
+++ bin/bin/poweroff.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 rm -rf ~/.cache/* 
-~/bin/cb.sh
 ~/bin/bup.sh
 ~/bin/gpush.sh
 pkill sleep
 rm /var/tmp/sync.sh.flag
 rm /var/tmp/gitpull.sh.flag
 
-poweroff
+loginctl poweroff
